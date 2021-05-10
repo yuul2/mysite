0612bc77a455c5145ff33c12b61e68c0eb0b33a8
youazzang <dbdkrla123@gmail.com>
2021-05-10 15:15:29 +0900

    recently

--- soar/assets/js/main.js
+++ soar/assets/js/main.js
@@ -41,17 +41,17 @@
   });
 
   // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
+  // $(document).ready(function() {
+  //   if (window.location.hash) {
+  //     var initial_nav = window.location.hash;
+  //     if ($(initial_nav).length) {
+  //       var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+  //       $('html, body').animate({
+  //         scrollTop: scrollto
+  //       }, 1500, 'easeInOutExpo');
+  //     }
+  //   }
+  // });
 
   // Mobile Navigation
   if ($('.nav-menu').length) {
